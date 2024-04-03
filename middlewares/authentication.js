2eaa2fba0f689deb9d42782b16ec9a1ef1693d50
Shradhesh_jain <shradeshjain123@gmail.com>
2024-04-03 13:47:57 +0530

    token

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,24 +1,54 @@
-const { validateToken } = require("../controllers/authentication");
+// const { validateToken } = require("../controllers/authentication");
 
-function checkForAuthenticationCookie(cookieName) {
-  return (req, res, next) => {
-    const tokenCookieValue = req.cookies[cookieName];
-    if (!tokenCookieValue) {
-      return next();
+// function checkForAuthenticationCookie(cookieName) {
+//   return (req, res, next) => {
+//     const tokenCookieValue = req.cookies[cookieName];
+//     if (!tokenCookieValue) {
+//       return next();
+//     }
+
+//     try {
+//       const userPayload = validateToken(tokenCookieValue);
+//       req.user = userPayload;
+//     } catch (error) {
+//       console.log(error);
+//     }
+
+//     return next();
+//   };
+// }
+
+// module.exports = {
+//   checkForAuthenticationCookie,
+// };
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const checkForAuthenticationCookie = async (req, res, next) => {
+    const token = req.headers['x-auth-token'];
+    if (!token) {
+        return res.status(401).json({
+        errors: [
+            {
+                msg: "Token not found",
+            },
+        ],
+        });
     }
 
     try {
-      const userPayload = validateToken(tokenCookieValue);
-      // console.log("check for auth cookie: ",userPayload)
-      req.user = userPayload;
-    } catch (error) {
-      console.log(error);
+        const user =  jwt.verify(token, process.env.SECERT_JWT_TOKEN);
+        req.userAuth=user   
+        next();
+    }catch (error) {
+        return res.status(403).json({
+        errors: [
+            {
+                msg: "Invalid token",
+            },
+        ],
+        });
     }
-
-    return next();
-  };
-}
-
-module.exports = {
-  checkForAuthenticationCookie,
 };
+
+module.exports = checkForAuthenticationCookie;
