7fa165af1a6bcbb8192a2af7d18036a3f8c1f0a0
Shradhesh_jain <shradeshjain123@gmail.com>
2024-04-04 01:40:48 +0530

    payload update

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,9 +3,12 @@ const JWT = require("jsonwebtoken");
 const secret = process.env.SECERT_JWT_TOKEN;
 
 function createTokenForUser(user) {
+  console.log(user);
   const payload = {
     id: user._id,
     email: user.email,
+    isVoted: user.isVoted,
+    createdAt: user.createdAt,
   };
   const token = JWT.sign(payload, secret, {
     expiresIn: "6h",
